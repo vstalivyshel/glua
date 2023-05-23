7ad518e5168728ccfab5710b33e8682057b11030
vstalivyshel <112723363+vstalivyshel@users.noreply.github.com>
2023-05-23 14:37:33 +0300

    rustfmt

--- glua/src/main.rs
+++ glua/src/main.rs
@@ -1,20 +1,20 @@
 use std::{
     collections::VecDeque,
-    fs,
-    path::Path,
     env,
     ffi::CString,
-    io::{Read, BufRead, BufWriter, BufReader, Result, Write, self},
+    fs,
+    io::{self, BufRead, BufReader, BufWriter, Read, Result, Write},
+    path::Path,
     process::{Command, Stdio},
 };
 
 extern "C" {
-    fn mkfifo(path: *const i8,  mode: u32) -> i32;
+    fn mkfifo(path: *const i8, mode: u32) -> i32;
     fn daemon(nochdir: i32, noclose: i32) -> i32;
 }
 
 struct TempFile {
-	path: String,
+    path: String,
 }
 
 impl From<&str> for TempFile {
@@ -32,10 +32,8 @@ impl TempFile {
             path.push('X');
         }
 
-        Self {
-            path
-        }
-    } 
+        Self { path }
+    }
 }
 
 impl Drop for TempFile {
@@ -47,7 +45,7 @@ impl Drop for TempFile {
 fn daemonize() -> Result<()> {
     if unsafe { daemon(0, 0) != 0 } {
         return Err(io::Error::last_os_error());
-    } 
+    }
 
     Ok(())
 }
@@ -62,7 +60,7 @@ fn temp_fifo() -> Result<TempFile> {
 fn create_fifo(path: &str, mode: u32) -> Result<()> {
     let path = CString::new(path).unwrap();
     if unsafe { mkfifo(path.as_ptr(), mode) != 0 } {
-        return Err(io::Error::last_os_error().into());
+        return Err(io::Error::last_os_error());
     }
 
     Ok(())
@@ -92,39 +90,40 @@ fn run() -> Result<()> {
     let mut args = env::args().skip(1).collect::<VecDeque<String>>();
     let args_len = args.len();
     if args_len < 1 {
-        return Ok(eprintln!("fail wrong arument count"));
+        return Ok(eprintln!("fail wrong argument count"));
     }
 
     match args.pop_front().unwrap().as_str() {
-        "pipe" => if args_len >= 1 {
-            let fifo = temp_fifo()?;
-            println!("{p}", p = &fifo.path);
-            let mut cmd = Command::new(&args.pop_front().unwrap())
-                .args(args.make_contiguous())
-                .stdout(Stdio::piped())
-                .stderr(Stdio::piped())
-                .spawn()?;
-			let fifo_path = Path::new(&fifo.path);
+        "pipe" => {
+            if args_len >= 1 {
+                let fifo = temp_fifo()?;
+                println!("{p}", p = &fifo.path);
+                let mut cmd = Command::new(&args.pop_front().unwrap())
+                    .args(args.make_contiguous())
+                    .stdout(Stdio::piped())
+                    .stderr(Stdio::piped())
+                    .spawn()?;
+                let fifo_path = Path::new(&fifo.path);
 
-            let stdout = cmd.stdout.take().unwrap();
-            let stderr = cmd.stderr.take().unwrap();
-            let reader = BufReader::new(stdout.chain(stderr));
+                let stdout = cmd.stdout.take().unwrap();
+                let stderr = cmd.stderr.take().unwrap();
+                let reader = BufReader::new(stdout.chain(stderr));
 
-			daemonize()?;
-			let fifo: fs::File;
-			match fs::OpenOptions::new().write(true).open(&fifo_path) {
-                Err(_) => return Ok(()),
-                Ok(f) => fifo = f,
-			}
-
-            let mut writer = BufWriter::new(fifo);
-            for out in reader.split(b'\n') {
-                let mut out = match out {
-                    Ok(o) => o,
-                    Err(e) => e.to_string().into_bytes(),
+                daemonize()?;
+                let fifo: fs::File = match fs::OpenOptions::new().write(true).open(fifo_path) {
+                    Err(_) => return Ok(()),
+                    Ok(f) => f,
                 };
-                out.push(b'\n');
-                let _ = writer.write_all(out.as_slice());
+
+                let mut writer = BufWriter::new(fifo);
+                for out in reader.split(b'\n') {
+                    let mut out = match out {
+                        Ok(o) => o,
+                        Err(e) => e.to_string().into_bytes(),
+                    };
+                    out.push(b'\n');
+                    let _ = writer.write_all(out.as_slice());
+                }
             }
         }
         "eval" if args_len >= 2 => kak_send_msg(
@@ -165,6 +164,6 @@ fn run() -> Result<()> {
 
 fn main() {
     if let Err(some_er) = run() {
-        println!("fail {some_er}");
+        eprintln!("fail {some_er}");
     }
 }
